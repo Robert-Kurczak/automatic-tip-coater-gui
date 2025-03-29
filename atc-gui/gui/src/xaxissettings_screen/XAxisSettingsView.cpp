cd0b774b47acdef04d921095d0846d1cefab6981
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-03-30 00:40:31 +0100

    Added ui handlers to start position on x axis changes

--- atc-gui/gui/src/xaxissettings_screen/XAxisSettingsView.cpp
+++ atc-gui/gui/src/xaxissettings_screen/XAxisSettingsView.cpp
@@ -14,3 +14,40 @@ void XAxisSettingsView::tearDownScreen()
 {
     XAxisSettingsViewBase::tearDownScreen();
 }
+
+void XAxisSettingsView::increaseStartPositionPressed()
+{
+    if (startPosition <= maxPosition - positionIncrement)
+    {
+        startPosition += positionIncrement;
+    }
+
+    updateStartPositionValue();
+}
+
+void XAxisSettingsView::decreaseStartPositionPressed()
+{
+    if (startPosition >= positionIncrement)
+    {
+        startPosition -= positionIncrement;
+    }
+
+    updateStartPositionValue();
+}
+
+void XAxisSettingsView::applyStartPositionPressed()
+{
+
+}
+
+void XAxisSettingsView::updateStartPositionValue()
+{
+    Unicode::snprintf(
+        startPositionTextBuffer,
+        STARTPOSITIONTEXT_SIZE,
+        "%d",
+        startPosition
+    );
+
+	startPositionText.invalidate();
+}
