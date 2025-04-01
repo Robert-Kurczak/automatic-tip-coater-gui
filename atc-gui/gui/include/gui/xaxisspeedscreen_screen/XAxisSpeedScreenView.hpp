a15efd5138ef708dfa7570a12b87e49cfc9caad7
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-04-02 00:15:43 +0200

    Added implementation of x axis speed settings buttons

--- atc-gui/gui/include/gui/xaxisspeedscreen_screen/XAxisSpeedScreenView.hpp
+++ atc-gui/gui/include/gui/xaxisspeedscreen_screen/XAxisSpeedScreenView.hpp
@@ -12,6 +12,16 @@ public:
     virtual void setupScreen();
     virtual void tearDownScreen();
 protected:
+    virtual void increaseButtonPressed() override;
+    virtual void decreaseButtonPressed() override;
+    virtual void saveButtonPressed() override;
+
+private:
+    static const uint8_t minSpeed = 0;
+    static const uint8_t maxSpeed = 255;
+    static const uint8_t speedIncrement = 1;
+
+    uint16_t speed = 0;
 };
 
 #endif // XAXISSPEEDSCREENVIEW_HPP
