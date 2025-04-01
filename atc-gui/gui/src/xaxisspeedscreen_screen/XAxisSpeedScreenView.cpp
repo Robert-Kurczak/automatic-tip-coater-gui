8cf73d4a20406d1ffac478b169627b3dc64c5bcb
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-04-02 00:15:43 +0200

    Added implementation of x axis speed settings buttons

--- atc-gui/gui/src/xaxisspeedscreen_screen/XAxisSpeedScreenView.cpp
+++ atc-gui/gui/src/xaxisspeedscreen_screen/XAxisSpeedScreenView.cpp
@@ -1,8 +1,11 @@
 #include <gui/xaxisspeedscreen_screen/XAxisSpeedScreenView.hpp>
 
+static uint8_t savedSpeed = 0;
+
 XAxisSpeedScreenView::XAxisSpeedScreenView()
 {
-
+    speed = savedSpeed;
+    xAxisSpeedSelector.setDisplayedValue(savedSpeed);
 }
 
 void XAxisSpeedScreenView::setupScreen()
@@ -13,4 +16,27 @@ void XAxisSpeedScreenView::setupScreen()
 void XAxisSpeedScreenView::tearDownScreen()
 {
     XAxisSpeedScreenViewBase::tearDownScreen();
+}
+
+void XAxisSpeedScreenView::increaseButtonPressed()
+{
+    if (speed <= maxSpeed - speedIncrement)
+    {
+        speed += speedIncrement;
+        xAxisSpeedSelector.setDisplayedValue(speed);
+    }
+}
+
+void XAxisSpeedScreenView::decreaseButtonPressed()
+{
+    if (speed >= speedIncrement)
+    {
+        speed -= speedIncrement;
+        xAxisSpeedSelector.setDisplayedValue(speed);
+    }
+}
+
+void XAxisSpeedScreenView::saveButtonPressed()
+{
+    savedSpeed = speed;
 }
