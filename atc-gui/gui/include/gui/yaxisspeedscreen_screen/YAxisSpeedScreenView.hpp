e3b1fc83e2d25e5842d067a9d159744b8bc7f79f
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-04-04 02:16:50 +0200

    Implemented y axis settings handler

--- atc-gui/gui/include/gui/yaxisspeedscreen_screen/YAxisSpeedScreenView.hpp
+++ atc-gui/gui/include/gui/yaxisspeedscreen_screen/YAxisSpeedScreenView.hpp
@@ -11,7 +11,18 @@ public:
     virtual ~YAxisSpeedScreenView() {}
     virtual void setupScreen();
     virtual void tearDownScreen();
+
 protected:
+    virtual void increaseButtonPressed() override;
+    virtual void decreaseButtonPressed() override;
+    virtual void saveButtonPressed() override;
+
+private:
+    static const uint8_t minSpeed = 0;
+    static const uint8_t maxSpeed = 255;
+    static const uint8_t speedIncrement = 1;
+
+    uint16_t speed = 0;
 };
 
 #endif // YAXISSPEEDSCREENVIEW_HPP
--- atc-gui/gui/src/yaxisspeedscreen_screen/YAxisSpeedScreenView.cpp
+++ atc-gui/gui/src/yaxisspeedscreen_screen/YAxisSpeedScreenView.cpp
@@ -1,8 +1,11 @@
 #include <gui/yaxisspeedscreen_screen/YAxisSpeedScreenView.hpp>
 
+static uint8_t savedSpeed = 0;
+
 YAxisSpeedScreenView::YAxisSpeedScreenView()
 {
-
+    speed = savedSpeed;
+    axisSpeedSelector.setDisplayedValue(savedSpeed);
 }
 
 void YAxisSpeedScreenView::setupScreen()
@@ -13,4 +16,28 @@ void YAxisSpeedScreenView::setupScreen()
 void YAxisSpeedScreenView::tearDownScreen()
 {
     YAxisSpeedScreenViewBase::tearDownScreen();
+}
+
+
+void YAxisSpeedScreenView::increaseButtonPressed()
+{
+    if (speed <= maxSpeed - speedIncrement)
+    {
+        speed += speedIncrement;
+        axisSpeedSelector.setDisplayedValue(speed);
+    }
+}
+
+void YAxisSpeedScreenView::decreaseButtonPressed()
+{
+    if (speed >= speedIncrement)
+    {
+        speed -= speedIncrement;
+        axisSpeedSelector.setDisplayedValue(speed);
+    }
+}
+
+void YAxisSpeedScreenView::saveButtonPressed()
+{
+    savedSpeed = speed;
 }
