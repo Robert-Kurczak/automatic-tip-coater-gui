46f5b98fc1f0b92f2f6d097a162f2c247460d187
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-05-25 19:02:48 +0200

    Added read and write methods to FMC module

--- application/FlexibleMemoryController/FlexibleMemoryController.hpp
+++ application/FlexibleMemoryController/FlexibleMemoryController.hpp
@@ -6,7 +6,7 @@ class FlexibleMemoryController {
 private:
     volatile uint16_t* registerAddress;
     volatile uint16_t* dataAddress;
-    
+
 public:
     FlexibleMemoryController(
         const uintptr_t registerAddress_,
@@ -15,4 +15,8 @@ public:
 
     void writeRegister(const uint16_t value);
     void writeData(const uint16_t value);
+    void write(const uint16_t reg, const uint16_t data);
+
+    uint16_t readData() const;
+    uint16_t read(const uint16_t reg) const;
 };
