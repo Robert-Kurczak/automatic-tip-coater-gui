ae077a394bcd3ce368370f8fe05148dc10f004d1
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-06-02 00:14:13 +0200

    Integrated touch controller with touchgfx

--- atc-nucleo-u5a5zj/TouchGFX/target/STM32TouchController.cpp
+++ atc-nucleo-u5a5zj/TouchGFX/target/STM32TouchController.cpp
@@ -23,13 +23,45 @@
 /* USER CODE BEGIN STM32TouchController */
 
 #include <STM32TouchController.hpp>
+#include <stdio.h>
+#include "application/TouchControllerXpt2046/TouchControllerXpt2046.hpp"
+#include "main.h"
+
+extern SPI_HandleTypeDef hspi1;
+extern UART_HandleTypeDef huart1;
+
+Spi spi {hspi1};
+GpioPin touchControllerChipSelectPin {
+    *TouchPanel_CS_GPIO_Port,
+    TouchPanel_CS_Pin
+};
+GpioPin touchControllerInterruptPin {
+    *TouchPanel_IRQ_GPIO_Port,
+    TouchPanel_IRQ_Pin
+};
+TouchControllerXpt2046 touchController {
+    spi,
+    touchControllerChipSelectPin,
+    touchControllerInterruptPin,
+    480,
+    800,
+    130,
+    1900,
+    80,
+    1950
+};
+
+void debug_print(const char* string) {
+    while (*string) {
+        while (__HAL_UART_GET_FLAG(&huart1, UART_FLAG_TXE) == RESET)
+            ;
+        HAL_UART_Transmit(&huart1, (const uint8_t*) &(*string++), 1, HAL_MAX_DELAY);
+    }
+}
 
 void STM32TouchController::init()
 {
-    /**
-     * Initialize touch controller and driver
-     *
-     */
+  touchController.init();
 }
 
 bool STM32TouchController::sampleTouch(int32_t& x, int32_t& y)
@@ -44,6 +76,25 @@ bool STM32TouchController::sampleTouch(int32_t& x, int32_t& y)
      * By default sampleTouch is called every tick, this can be adjusted by HAL::setTouchSampleRate(int8_t);
      *
      */
+
+    if (touchController.isTouched()) {
+      x = touchController.readX();
+      y = 800 - touchController.readY();
+
+      char str[256];
+      sprintf(
+          str,
+          "x: %d, y: %d | x: %dpx, y: %dpx\r\n",
+          touchController.readRawX(),
+          touchController.readRawY(),
+          touchController.readX(),
+          touchController.readY()
+      );
+      debug_print(str);
+
+      return true;
+    }
+
     return false;
 }
 
