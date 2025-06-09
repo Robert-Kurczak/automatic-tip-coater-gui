b38c811235f3020703cc0f226db2388c7ee0a9b5
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-06-09 22:15:04 +0200

    Wrapped every module in ATC namespace

--- application/DelayProvider/DelayProvider.cpp
+++ application/DelayProvider/DelayProvider.cpp
@@ -5,7 +5,9 @@
 // TODO remove this dependency
 #include "../../atc-nucleo-u5a5zj/Core/Inc/main.h"
 
+namespace ATC {
 void DelayProvider::delayMiliseconds(const uint32_t value) {
     // TODO abstract it out
     HAL_Delay(value);
+}
 }
