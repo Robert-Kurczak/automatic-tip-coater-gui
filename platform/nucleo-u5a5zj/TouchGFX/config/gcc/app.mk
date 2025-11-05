1265a7376bcc27a4af6e7e6ea693a649b5926218
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-11-05 01:10:22 +0100

    Rolled back touchgfx to 4.25 because 4.26 is a buggy mess

--- platform/nucleo-u5a5zj/TouchGFX/config/gcc/app.mk
+++ platform/nucleo-u5a5zj/TouchGFX/config/gcc/app.mk
@@ -3,7 +3,7 @@
 touchgfx_path := ../Middlewares/ST/touchgfx
 
 # Location of the TouchGFX Environment
-touchgfx_env := ../../../../../Programs/TouchGfx/env
+touchgfx_env := ../../../../../TouchGFX/4.25.0/env
 # Optional additional compiler flags
 user_cflags := -std=gnu++20 -DUSE_BPP=16 -Wformat-truncation=0
 linker_options := -no-pie
