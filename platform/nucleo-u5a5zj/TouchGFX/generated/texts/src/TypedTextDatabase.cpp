e0e515d2c391f1040c4c3a98d4171c63336854be
Robert Kurczak <robertkurczak.studia@gmail.com>
2025-11-06 01:49:23 +0100

    Specifying application directory path in touchgfx simulator makefile

--- platform/nucleo-u5a5zj/TouchGFX/generated/texts/src/TypedTextDatabase.cpp
+++ platform/nucleo-u5a5zj/TouchGFX/generated/texts/src/TypedTextDatabase.cpp
@@ -157,7 +157,8 @@ const touchgfx::TypedText::TypedTextData typedText_database_DEFAULT[] TEXT_LOCAT
     { 0, touchgfx::CENTER, touchgfx::TEXT_DIRECTION_LTR }
 };
 
-const touchgfx::TypedText::TypedTextData* const typedTextDatabaseArray[] = {
+TEXT_LOCATION_FLASH_PRAGMA
+const touchgfx::TypedText::TypedTextData* const typedTextDatabaseArray[] TEXT_LOCATION_FLASH_ATTRIBUTE = {
     typedText_database_DEFAULT
 };
 
