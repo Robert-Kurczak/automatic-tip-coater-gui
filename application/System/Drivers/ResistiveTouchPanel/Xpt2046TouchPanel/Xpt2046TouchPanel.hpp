6827a8975f90a676d91577389fcf09061a45c63c
Robert Kurczak <robertkurczak.studia@gmail.com>
2026-01-09 11:06:40 +0100

    Fixed warnings in TouchPanel module

--- application/System/Drivers/ResistiveTouchPanel/Xpt2046TouchPanel/Xpt2046TouchPanel.hpp
+++ application/System/Drivers/ResistiveTouchPanel/Xpt2046TouchPanel/Xpt2046TouchPanel.hpp
@@ -5,32 +5,30 @@
 #include "application/System/Ports/ISpi.hpp"
 
 namespace ATC {
+
+// NOLINTBEGIN(cppcoreguidelines-avoid-const-or-ref-data-members)
 struct Xpt2046TouchPanelPinout {
     IGpioPin& chipSelectPin;
     IGpioPin& touchInterruptPin;
 };
+// NOLINTEND(cppcoreguidelines-avoid-const-or-ref-data-members)
 
 class Xpt2046TouchPanel : public IResistiveTouchPanel {
 private:
     Xpt2046TouchPanelPinout& pinout_;
     ISpi& spi_;
 
-    static constexpr uint8_t READ_X_COMMAND_ = 0xD0;
-    static constexpr uint8_t READ_Y_COMMAND_ = 0x90;
-    static constexpr uint8_t READ_Z1_COMMAND_ = 0xB0;
-    static constexpr uint8_t READ_Z2_COMMAND_ = 0xC0;
-
     uint16_t transferReadCommand(uint8_t command);
 
 public:
     Xpt2046TouchPanel(Xpt2046TouchPanelPinout& pinout, ISpi& spi);
 
-    virtual void init() override;
+    void init() override;
 
-    virtual bool isTouchDetected() override;
+    bool isTouchDetected() override;
 
-    virtual uint16_t readRawX() override;
-    virtual uint16_t readRawY() override;
-    virtual uint16_t readRawZ() override;
+    uint16_t readRawX() override;
+    uint16_t readRawY() override;
+    uint16_t readRawZ() override;
 };
 }
