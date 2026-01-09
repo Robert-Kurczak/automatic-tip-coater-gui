b0c6da09ed474cfbcd4e5f7488b2c5bbd7fc526b
Robert Kurczak <robertkurczak.studia@gmail.com>
2026-01-09 14:03:05 +0100

    Fixed warnings in SpindleController module

--- application/System/Controllers/SpindleController/ISpindleController.hpp
+++ application/System/Controllers/SpindleController/ISpindleController.hpp
@@ -2,17 +2,22 @@
 
 #include "application/System/Controllers/PersistentStorageController/PersistentData/SpindlePersistentConfig.hpp"
 
-#include <stdint.h>
+#include <cstdint>
 
 namespace ATC {
 class ISpindleController {
 public:
+    ISpindleController() = default;
+    ISpindleController(const ISpindleController&) = delete;
+    ISpindleController& operator=(const ISpindleController&) = delete;
+    ISpindleController(ISpindleController&&) = delete;
+    ISpindleController& operator=(ISpindleController&&) = delete;
     virtual ~ISpindleController() = default;
 
     virtual void init(const SpindlePersistentConfig& config) = 0;
     virtual void tick() = 0;
 
-    virtual bool wasFaultReported() = 0;
+    [[nodiscard]] virtual bool wasFaultReported() = 0;
 
     virtual void startRotation() = 0;
 
@@ -21,16 +26,16 @@ public:
 
     virtual void stopRotation() = 0;
 
-    virtual bool isTimedRotationFinished() const = 0;
+    [[nodiscard]] virtual bool isTimedRotationFinished() const = 0;
 
     virtual void setDirectionClockwise() = 0;
     virtual void setDirectionCounterClockwise() = 0;
-    virtual bool isDirectionClockwise() const = 0;
+    [[nodiscard]] virtual bool isDirectionClockwise() const = 0;
 
     virtual void setSpeedPercent(uint8_t value) = 0;
-    virtual uint8_t getSpeedPercent() const = 0;
+    [[nodiscard]] virtual uint8_t getSpeedPercent() const = 0;
 
     virtual void setRotationTimeInMillis(uint32_t value) = 0;
-    virtual uint32_t getRotationTimeInMillis() const = 0;
+    [[nodiscard]] virtual uint32_t getRotationTimeInMillis() const = 0;
 };
 }
