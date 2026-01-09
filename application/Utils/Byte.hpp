2f3b15141d45912a39b1d9bc924f648270a70a6d
Robert Kurczak <robertkurczak.studia@gmail.com>
2026-01-09 21:23:11 +0100

    Fixed warnings in utils

--- application/Utils/Byte.hpp
+++ application/Utils/Byte.hpp
@@ -1,32 +1,35 @@
 #pragma once
 
+#include <cstdint>
 #include <cstring>
 #include <span>
-#include <stdint.h>
 
 namespace ATC {
 template<typename Type>
 std::span<const uint8_t> toByteSpan(const Type& data) {
-    static_assert(std::is_trivially_copyable<Type>::value);
+    static_assert(std::is_trivially_copyable_v<Type>);
 
     return std::span<const uint8_t> {
-        reinterpret_cast<const uint8_t*>(&data), sizeof(Type)
+        // NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast)
+        reinterpret_cast<const uint8_t*>(&data),
+        sizeof(Type)
+        // NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast)
     };
 }
 
 template<typename Type>
 Type fromByteSpan(const std::span<const uint8_t>& bytes) {
-    static_assert(std::is_trivially_copyable<Type>::value);
+    static_assert(std::is_trivially_copyable_v<Type>);
 
     Type structure {};
-    std::memcpy(&structure, bytes.data(), sizeof(Type));
+    std::memcpy(&structure, bytes.data(), sizeof(Type)); // NOLINT
 
     return structure;
 }
 
 template<typename Type>
 bool areBytesEqual(const Type& left, const Type& right) {
-    static_assert(std::is_trivially_copyable<Type>::value);
+    static_assert(std::is_trivially_copyable_v<Type>);
 
     return std::memcmp(&left, &right, sizeof(Type)) == 0;
 }
