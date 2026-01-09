aa17868801333d90ff97a59e33cbe01c8db24cfe
Robert Kurczak <robertkurczak.studia@gmail.com>
2026-01-09 14:56:24 +0100

    Fixed warnings in ports

--- application/System/Ports/IGpioPin.hpp
+++ application/System/Ports/IGpioPin.hpp
@@ -3,12 +3,17 @@
 namespace ATC {
 class IGpioPin {
 public:
+    IGpioPin() = default;
+    IGpioPin(const IGpioPin&) = delete;
+    IGpioPin& operator=(const IGpioPin&) = delete;
+    IGpioPin(IGpioPin&&) = delete;
+    IGpioPin& operator=(IGpioPin&&) = delete;
     virtual ~IGpioPin() = default;
 
     virtual void setInputMode() = 0;
     virtual void setInputPullUpMode() = 0;
     virtual void setInputPullDownMode() = 0;
-    virtual bool isHigh() = 0;
+    [[nodiscard]] virtual bool isHigh() = 0;
 
     virtual void setOutputMode() = 0;
     virtual void setHigh() = 0;
--- application/System/Ports/IUart.hpp
+++ application/System/Ports/IUart.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <span>
-#include <stdint.h>
 
 namespace ATC {
 class IUart {
 public:
+    IUart() = default;
+    IUart(const IUart&) = delete;
+    IUart& operator=(const IUart&) = delete;
+    IUart(IUart&&) = delete;
+    IUart& operator=(IUart&&) = delete;
     virtual ~IUart() = default;
 
     virtual void write(const std::span<const char>& data) = 0;
