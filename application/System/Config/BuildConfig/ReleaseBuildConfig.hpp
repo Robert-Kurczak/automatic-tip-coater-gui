fdac8cd79e217931a5201b28c0be42c69b189d5f
Robert Kurczak <robertkurczak.studia@gmail.com>
2026-01-24 23:10:17 +0100

    Grouped related configs together

--- application/System/Config/BuildConfig/ReleaseBuildConfig.hpp
+++ application/System/Config/BuildConfig/ReleaseBuildConfig.hpp
@@ -1,4 +1,6 @@
 #include <cstdint>
 
+namespace ATC {
 inline constexpr bool LOGGER_ENABLED = false;
 inline constexpr uint32_t LOG_BUFFER_SIZE = 1;
+}
--- application/System/Config/DebugBuildConfig.hpp
+++
@@ -1,4 +0,0 @@
-#include <cstdint>
-
-inline constexpr bool LOGGER_ENABLED = true;
-inline constexpr uint32_t LOG_BUFFER_SIZE = 1024;
